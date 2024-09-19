5225955b46be2c92ecc1a33ddb10632d47f818db
XDSSWAR <xdsswar@gmail.com>
2024-09-19 11:21:54 -0400

    First Commit

--- src/main/java/xss/it/backend/entity/User.java
+++ src/main/java/xss/it/backend/entity/User.java
@@ -37,5 +37,5 @@ public class User {
     private String password;
 
     @Lob
-    private byte[] avatar; // We need to add @Lob support by extending the Dialect, so then we can get the bytes of the image
+    private byte[] avatar;
 }
